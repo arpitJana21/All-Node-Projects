fcaded54eaf5622081c20454b1f9dabe669079b5
Arpit Jana <arpitjana2103@gmail.com>
2023-07-08 20:26:37 +0530

    Refactored

--- Masai/12 Redis/model/user.model.js
+++ Masai/12 Redis/model/user.model.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-
+const UserSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        pass: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: 'User',
+            enum: ['User', 'Moderator'],
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    pass: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'User',
-        enum: ['User', 'Moderator']
+    {
+        versionKey: false,
     }
-},{
-    versionKey: false
-})
-
+);
 
 const UserModel = mongoose.model('user', UserSchema);
 module.exports = {UserModel};
