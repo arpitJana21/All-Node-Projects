ce6a6b0e02df09d53497642b1f1acd410f54b435
Arpit Jana <arpitjana2103@gmail.com>
2023-07-13 20:46:59 +0530

    commit

--- YT Coders Gyan/Node Crash Course/auth.js
+++
@@ -1,9 +0,0 @@
-const register = function (username) {
-    console.log(`User ${username} has been registered.`);
-};
-
-const login = function (username) {
-    console.log(`User ${username} has been logged in.`);
-};
-
-module.exports = {register, login};
