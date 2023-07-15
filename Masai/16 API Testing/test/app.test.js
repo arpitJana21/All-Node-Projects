6fe6196ab3f110fa35f93aee405d92fcd686f9a4
Arpit Jana <arpitjana2103@gmail.com>
2023-07-15 22:57:01 +0530

    get buy User ID test case written

--- Masai/16 API Testing/test/app.test.js
+++ Masai/16 API Testing/test/app.test.js
@@ -21,6 +21,25 @@ describe('Testing APIs', function () {
                 });
         });
     });
+
+    describe('GET /users/:userID', function () {
+        it('should GET the user by the ID', function (done) {
+            const userID = 6;
+            chai.request(server)
+                .get(`/user/${userID}`)
+                .end(function (err, response) {
+                    response.should.have.status(200);
+                    response.body.should.be.a("object");
+                    response.body.should.have.property("id").eq(6);
+                    response.body.should.have.property("name")
+                    response.body.should.have.property("userName")
+                    response.body.should.have.property("email")
+
+                    score += 2;
+                    done();
+                });
+        });
+    });
 });
 
 console.log("Final Score : " + score);
