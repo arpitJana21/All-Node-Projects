837a7273e0f19c046350f73373c5fcb97cc47e3b
Arpit Jana <arpitjana2103@gmail.com>
2023-07-16 09:41:37 +0530

    Patch Route Added

--- Masai/16 API Testing/test/app.test.js
+++ Masai/16 API Testing/test/app.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 
-const { server } = require('../server.js');
+const {server} = require('../server.js');
 const chaiHttp = require('chai-http');
 chai.should();
 chai.use(chaiHttp);
@@ -14,14 +14,14 @@ describe('Testing APIs', function () {
                 .get('/users')
                 .end(function (err, response) {
                     response.should.have.status(200);
-                    response.body.should.be.a("array");
+                    response.body.should.be.a('array');
                     response.body.should.have.length.above(9);
                     score += 2;
                     done();
                 });
         });
     });
-
+    // Testing GET with UserID Route
     describe('GET /users/:userID', function () {
         it('should GET the user by the ID', function (done) {
             const userID = 6;
@@ -29,11 +29,36 @@ describe('Testing APIs', function () {
                 .get(`/users/${userID}`)
                 .end(function (err, response) {
                     response.should.have.status(200);
-                    response.body.should.be.a("object");
-                    response.body.should.have.property("id").eq(6);
-                    response.body.should.have.property("name")
-                    response.body.should.have.property("userName")
-                    response.body.should.have.property("email")
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('id').eq(6);
+                    response.body.should.have.property('name');
+                    response.body.should.have.property('userName');
+                    response.body.should.have.property('email');
+
+                    score += 2;
+                    done();
+                });
+        });
+    });
+    // Testing Patch
+    describe('GET /users/update/:userID', function () {
+        it('should PATCH the existing user', function (done) {
+            const userID = 1;
+            const user = {
+                userName: 'cool_kid',
+            };
+            chai.request(server)
+                .patch(`/users/update/${userID}`)
+                .send(user)
+                .end(function (err, response) {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('id').eq(1);
+                    response.body.should.have.property('name');
+                    response.body.should.have
+                        .property('userName')
+                        .eq('cool_kid');
+                    response.body.should.have.property('email');
 
                     score += 2;
                     done();
@@ -42,4 +67,4 @@ describe('Testing APIs', function () {
     });
 });
 
-console.log("Final Score : " + score);
+console.log('Final Score : ' + score);
