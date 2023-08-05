8ca280721518e06237d8c63bd36d5262c60fb520
Arpit Jana <arpitjana2103@gmail.com>
2023-08-05 20:58:31 +0530

    new markdown added

--- Masai/19 TypeScript/oops.ts
+++ Masai/19 TypeScript/oops.ts
@@ -1,12 +1,2 @@
-enum DayOfWeek {
-    Sunday = 'Sun',
-    Monday = 'Mon',
-    Tuesday = 'Tue',
-    Wednesday = 'Wed',
-    Thursday = 'Thu',
-    Friday = 'Fri',
-    Saturday = 'Sat',
-}
-
-let day: DayOfWeek = DayOfWeek.Wednesday;
-console.log(day); // Output: "Wed"
+// S.O.L.I.D
+// S = Single Respponsibility Principle
