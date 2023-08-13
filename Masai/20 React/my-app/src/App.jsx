a0f4c5197251aa1f33fbc32bcf6be7169ef476aa
Arpit Jana <arpitjana2103@gmail.com>
2023-08-13 11:02:55 +0530

    Used 1 State Insted of 3 States

--- Masai/20 React/my-app/src/App.jsx
+++ Masai/20 React/my-app/src/App.jsx
@@ -1,8 +1,8 @@
 // import Conditional from './Components/Conditional';
-import Form from './Components/Form';
+import Form2 from './Components/Form2';
 
 function App() {
-    return <Form />;
+    return <Form2 />;
 }
 
 export default App;
---
+++ Masai/20 React/my-app/src/Components/Form2.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+
+function From2() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(formData.name, formData.email, formData.password);
+    };
+
+    const handleInputChange = function (e) {
+        const {name, value} = e.target;
+        return setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    return (
+        <form onSubmit={submit}>
+            <div>
+                <span>Name</span>
+                <input
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    type="text"
+                    name="name"
+                />
+            </div>
+            <div>
+                <span>Email </span>
+                <input
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    type="email"
+                    name="email"
+                />
+            </div>
+            <div>
+                <span>Password </span>
+                <input
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    type="password"
+                    name="password"
+                />
+            </div>
+
+            <input type="submit" />
+        </form>
+    );
+}
+
+export default From2;
